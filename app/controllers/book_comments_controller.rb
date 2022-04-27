93a774257019fa1161c2bf843ce07e007af63450
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-04-27 13:06:33 +0000

    [Add]非同期通信追加中

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,21 +1,27 @@
 class BookCommentsController < ApplicationController
-  
-  def create 
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id 
-    comment.save
-    redirect_to request.referer
+
+  def create
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    @comment.save
+    # redirect_to request.referer
+    render :book_comments
   end
-  
+
   def destroy
     BookComment.find(params[:id]).destroy
-    redirect_to request.referer
+    # redirect_to request.referer
+    @book = Book.find(params[:book_id])
+    render :book_comments
   end
-  
+
   private
-  
+
   def book_comment_params
     params.require(:book_comment).permit(:comment)
   end
 end
+
+
+
